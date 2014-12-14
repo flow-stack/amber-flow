1fee1aa54c2dab636e5400fb31c8396e751dd111
Sebastian Sastre <sebastian@flowingconcept.com>
2014-12-14 00:03:22 -0200

    wip(sources): added the main current sources

--- devel.js
+++ devel.js
@@ -2,7 +2,11 @@ define([
     'amber/devel',
     './deploy',
     // --- packages used only during development begin here ---
-    'amber-amberflow/Amberflow-Tests'
+    'amber-minimapless/MiniMapless-Tests',
+   	'amber-amberflow/Flow-Binding-Tests',
+	'amber-amberflow/Flow-Templates-Tests',
+	'amber-amberflow/Flow-Tests',
+	'amber-amberflow/App-Tests'
     // --- packages used only during development end here ---
 ], function (amber) {
     return amber;
